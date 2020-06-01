92cb436a77fed7b93cc00ce26fb9a7af8df90294
shoaib doulatabad <shoaib.doulatabad@gmail.com>
2020-06-01 23:21:59 +0400

    complete the code or login an registor user and saving in DB

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,69 @@
-module.exports = function(_){
+module.exports = function(_, passport, User, validator){
   return {
     SetRouting : function(router){
       router.get('/', this.indexPage);
+      router.get('/signup', this.signupPage);
+      router.get('/home', this.homePage);
+
+       router.post('/', [
+          validator.check('email').not().isEmpty().isEmail()
+              .withMessage('Email is invalid'),
+          validator.check('password').not().isEmpty()
+              .withMessage('Password is required and must be at least 5 characters.'),
+        ], this.postValidation, this.postLogin);
+      // router.post('/signup', User.SignUpValidation,  this.postSignup);
+       router.post('/signup', [
+                validator.check('username').not().isEmpty().isLength({min: 5}).withMessage('Username is required and must be at least 5 characters.'),
+                validator.check('email').not().isEmpty().isEmail()
+                    .withMessage('Email is invalid'),
+                validator.check('password').not().isEmpty()
+                    .withMessage('Password is required and must be at least 5 characters.'),
+            ], this.postValidation, this.postSignup);
+    },
+    indexPage: function(req, res){
+      const errors = req.flash('error');
+      return res.render('index', {title: 'Sport-Chat | Login', messages: errors,
+        hasErrors: errors.length > 0});
+    },
+    signupPage: function(req, res){
+      const errors = req.flash('error');
+      return res.render('signup', {title: 'Sport-Chat | Login', messages: errors, hasErrors: errors.length > 0});
+    },
+    homePage: function(req, res){
+      return res.render('home');
     },
 
-    indexPage: function(req, res){
-      return res.render('index', {test: 'Hello world'});
-    }
+
+     postValidation: function(req, res, next) {
+        const err = validator.validationResult(req);
+        const reqErrors = err.array();
+        const errors = reqErrors.filter(e => e.msg !== 'Invalid value');
+        let messages = [];
+        errors.forEach((error) => {
+            messages.push(error.msg);
+        });
+        // req.flash('error', messages);
+        if (messages.length > 0) {
+            req.flash('error', messages);
+            if (req.url === '/signup') {
+                res.redirect('/signup');
+            } else if(req.url === '/') {
+                res.redirect('/');
+            }
+        }
+        return next();
+    },
+        
+
+    postSignup: passport.authenticate('local.signup', {
+            successRedirect: '/home',
+            failureRedirect: '/signup',
+            failureFlash: true
+    }),
+    postLogin: passport.authenticate('local.login', {
+        successRedirect: '/home',
+        failureRedirect: '/',
+        failureFlash: true
+    }),
   }
 }
--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
 
 const container = require('./container');
 
-container.resolve(function(users){
+container.resolve(function(users, _){
+  mongoose.Promise = global.Promise;
+  mongoose.set('useCreateIndex', true)
+  mongoose.connect('mongodb://localhost:27017/sport-chat',  { useUnifiedTopology: true, useNewUrlParser: true })
   const app = SetupExpress();
 
   function SetupExpress() {
@@ -26,12 +35,25 @@ container.resolve(function(users){
   }
 
   function ConfigureExpress(app){
+    require('./passport/passport-local');
     app.use(express.static('public'));
+    app.use(cookieParser());
     app.set('view engine', 'ejs');
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.use(session({
+      secret: 'thisisasecretkey',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({mongooseConnection: mongoose.connection})
+    }));
 
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.locals._ = _;
   }
 
 });
