d8e69e572ba2d08b4e18009b78cac971a365b819
shoaib doulatabad <shoaib.doulatabad@gmail.com>
2020-06-04 00:38:00 +0400

    added the first event for the client and server to pass the message between them

--- public/js/group/groupchat.js
+++ public/js/group/groupchat.js
@@ -4,4 +4,18 @@ $(document).ready(function(){
   socket.on('connect', () =>{
     console.log("aah user connected");
   })
+
+  $('#message-form').on('submit', function(e){
+    e.preventDefault();
+    
+    var msg = $('#msg').val();
+
+    socket.emit('createMessage',{
+      text: msg,
+    },function(){
+      $('#msg').val(' ');
+    })
+
+
+  });
 });
--- socket/groupchat.js
+++ socket/groupchat.js
@@ -2,5 +2,11 @@ module.exports = function(io){
 
   io.on('connection', (socket) =>{
     console.log('User Connected');
-  })
+    
+    
+    socket.on('createMessage', (message, callback) => {
+      console.log(message);
+    });
+
+  });
 }
